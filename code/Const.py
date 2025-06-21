70aa24eeba58335196672a916cc669364ca5670c
Abraão Castro <abraaoc901@gmail.com>
2025-06-21 15:00:36 -0300

    Implementação do Leve2

--- code/Const.py
+++ code/Const.py
@@ -8,6 +8,7 @@ C_GREEN = (0, 128, 0)
 C_CYAN = (0, 128, 128)
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -17,13 +18,18 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
     'Player1Shot': 1,
     'Player2': 3,
     'Player2Shot': 3,
-    'Enemy1': 2,
-    'Enemy2': 1,
+    'Enemy1': 1,
     'Enemy1Shot': 5,
+    'Enemy2': 1,
     'Enemy2Shot': 2,
 }
 
@@ -120,6 +126,10 @@ PLAYER_KEY_SHOOT = {'Player2': pygame.K_RCTRL,
 
 # S
 SPAWN_TIME = 4000
+
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 #W
 WIN_WIDTH = 576
