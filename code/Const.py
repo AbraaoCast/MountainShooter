fe5af4da4fd4edababb05ac47e87a5460471b2dd
Abraão Castro <abraaoc901@gmail.com>
2025-06-24 11:43:14 -0300

    Implementação do SQlite3 para o Score usando proxy design pattern

--- code/Const.py
+++ code/Const.py
@@ -124,13 +124,30 @@ PLAYER_KEY_RIGHT = {'Player2': pygame.K_RIGHT,
 PLAYER_KEY_SHOOT = {'Player2': pygame.K_RCTRL,
                     'Player1': pygame.K_LCTRL}
 
-# S
-SPAWN_TIME = 4000
 
 # T
 TIMEOUT_STEP = 100  # 100ms
 TIMEOUT_LEVEL = 20000  # 20s
 
-#W
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# S
+SPAWN_TIME = 4000
+
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
